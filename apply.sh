e162f261af0f2d823e472c922277910499a9e8d3
Nicholas Car <nicholas.car@surroundaustralia.com>
2021-03-09 23:42:20 +1000

    remove unused files

--- apply.sh
+++ apply.sh
@@ -25,21 +25,4 @@ fi
 echo "Add config file to VP"
 mv $VP_THEME_HOME/config_updated.py $VP_HOME/vocprez/_config/__init__.py
 
-# not used since the home page for all vocabularies is static
-#echo "Add endpoints to $VP_HOME/vocprez/app.py"
-## find the "# run the Flask app" and remove it and everything below
-#run_line=$(grep -n "# run the Flask app" $VP_HOME/vocprez/app.py | head -n 1 | cut -d: -f1)
-#run_line=$((run_line -1))
-#head -$run_line $VP_HOME/vocprez/app.py > $VP_THEME_HOME/app_temp.py
-
-#echo "Route mapping"
-#if `grep -q "# ROUTE vocabularies_set" "$VP_HOME/vocprez/app.py"`; then
-#    echo "already there"
-#else
-#    sed -n '/# END ROUTE cache_reload/q;p' $VP_HOME/vocprez/app.py > $VP_THEME_HOME/app_temp.py
-#    cat $VP_THEME_HOME/app_additions.py >> $VP_THEME_HOME/app_temp.py
-#    sed -e '1,/# run the Flask app/ d' $VP_HOME/vocprez/app.py >> $VP_THEME_HOME/app_temp.py
-#    mv $VP_THEME_HOME/app_temp.py $VP_HOME/vocprez/app.py
-#fi
-
 echo "customisation done"
